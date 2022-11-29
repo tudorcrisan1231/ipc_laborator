4ad3889adb146a6de140f339fcc6586c6b4af432
tudorcrisan1231 <crisantudor79@yahoo.com>
2022-11-29 19:23:46 +0200

    probleme ipc (CNP)

--- ipc_laborator2/lab9_2.c
+++ ipc_laborator2/lab9_2.c
@@ -1,214 +1,214 @@
-﻿//a) Şterge dintr - un şir de caractere un subşir specificat prin poziţie şi lungime.
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h> //pt stringuri
-#include<ctype.h> //pt functii ex toupper
-int validare(char nume[21]) {
-	int ok = 0, i;
-	if (nume[0] >= 'A' && nume[0] <= 'Z') {
-		for (i = 1; i < strlen(nume); i++) {
-			if (!(nume[i] >= 'a' && nume[i] <= 'z')) {
-				ok++;
-			}
-		}
-		if (ok == 0) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
-	else {
-		return 0;
-	}
-
-}
-void citire_sir(char** tenis, int n) {
-	int i;
-	char nume[21];
-	for (i = 0; i < n; i++) {
-		do {
-			printf("dati nume: ");
-			scanf("%s", nume);
-		} while (validare(nume) == 0);
-		tenis[i] = (char*)malloc(strlen(nume) + 1); //ii aloc memorie fiecarui cuvant din sir
-
-		if (tenis[i] == NULL) {
-			printf("Eroare alocare memorie");
-			exit(0);
-		}
-		else {
-			strcpy(tenis[i], nume); //bag in sir fiecare nume citit
-		}
-	}
-}
-
-void afisare_sir(char** tenis, int n) {
-	int i;
-	for (i = 0; i < n; i++) {
-		printf("%s ", *(tenis + i));
-	}
-	printf("\n");
-}
-void ord_alf(char** tenis, int n) {
-	int i,j;
-	char aux[21];
-	for(i=0;i<n;i++)
-		for (j = i + 1; j < n; j++) 
-			if (strcmp(*(tenis+i), *(tenis + j)) > 0) {
-				strcpy(aux, *(tenis + i));
-				strcpy(*(tenis + i), *(tenis + j));
-				strcpy(*(tenis + j),aux);
-			}
-	
-	for (i = 0; i < n; i++) {
-		printf("%s ", *(tenis + i));
-	}
-	printf("\n");
-}
-
-void ord_INValf(char** tenis, int n) {
-	int i, j;
-	char aux[21];
-	for (i = 0; i < n; i++)
-		for (j = i + 1; j < n; j++)
-			if (!(strcmp(*(tenis + i), *(tenis + j)) > 0)) {
-				strcpy(aux, *(tenis + i));
-				strcpy(*(tenis + i), *(tenis + j));
-				strcpy(*(tenis + j), aux);
-			}
-
-	for (i = 0; i < n; i++) {
-		printf("%s ", *(tenis + i));
-	}
-	printf("\n");
-}
-
-void nume_binar(char** tenis, int n) {
-	int i,aux;
-	for (i = 0; i < n; i++) {
-		aux = strlen(*(tenis + i));
-		while (aux > 0) {
-			printf("%d", aux % 2); 
-			aux = aux / 2;
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
-
-void ord_crescLung(char** tenis, int n) {
-	int i, j;
-	char aux[21];
-	for (i = 0; i < n; i++)
-		for (j = i + 1; j < n; j++)
-			if ( strlen(*(tenis + i))  > strlen(*(tenis + j)) ) {
-				strcpy(aux, *(tenis + i));
-				strcpy(*(tenis + i), *(tenis + j));
-				strcpy(*(tenis + j), aux);
-			}
-
-	for (i = 0; i < n; i++) {
-		printf("%s ", *(tenis + i));
-	}
-	printf("\n");
-}
-
-void tabelPointeriLungime(char** tenis, int n) {
-	int* tabel;
-	int i;
-	tabel = (int*)malloc(n * sizeof(int));
-	
-	for (i = 0; i < n; i++) {
-		*(tabel + i) = strlen(*(tenis+i));
-	}
-
-	for (i = 0; i < n; i++) {
-		printf("%d ", *(tabel + i));
-	}
-	printf("\n");
-}
-
-void matriceMax(char** tenis, int n) {
-	int max = strlen(*(tenis + 0)) , i,j;
-
-	for (i = 0; i < n; i++) {
-		if (max < strlen(*(tenis + i)) ) {
-			max = strlen(*(tenis + i));
-		}
-	}
-
-	for (i = 0; i < max; i++) {
-		for (j = 0; j < max; j++) {
-			if ((i == max - 1 && j == 0) || (i == max - 1 && j == max - 1)) {
-				printf("%d ", max);
-			}
-			else {
-				printf("0 ");
-			}
-		}
-		printf("\n");
-	}
-	//printf("%d",max);
-	printf("\n");
-}
-
-
-int main() {
-	//fie un sir fomrat din max 100 nume de tenismeni, realizati un program C in care se citesc numele de tenismen de la tastatura apoi realizati un meniu interactiv cu urm optiuni
-	//0. iesire
-	//1. afisare tenismeni cititi
-	//2. afisare tenismeni in ord alfabetica
-	//3. afisare tenismeni in ord invers alfabetica
-	//4. afisarea valorii binare a lungimii numelui fiecarui tenismen
-	//5. afisare in ord crescatoare dupa lungimea numelui fiecarui tenismen
-	//6. copierea intr un tabel de pointeri a lungimii fiecarui nume de tenismen
-	//7. afisarea sub forma de matrice a lungimii numelui cel mai mare in colturile de jos, iar in rest 0
-	//OBS: numele este validat fiind citit corect daca incepe cu litera mare, iar in rest contine doar litere mici
-	//     se pot citii max 100 nume de tensmen, iar fiecare tenismne are max 20 car.
-	//     se va folosi alocarea dinamica de memorie
-	//     pt fiecare  punct se va face o functie
-	int n,opt;
-	char** tenis;
-
-	do {
-		scanf("%d", &n);
-	} while (n < 0 || n > 100);
-	
-	tenis = (char**)malloc(n * sizeof(char*)); //aloc memorie
-
-	if (tenis == NULL) {
-		printf("Eroare alocare memorie");
-		exit(0);
-	}	
-
-	citire_sir(tenis, n);
-
-
-	while (1) {
-		printf("1. afisare tenismeni cititi \n");
-		printf("2. afisare tenismeni in ord alfabetica\n");
-		printf("3. afisare tenismeni in ord invers alfabetica\n");
-		printf("4. afisarea valorii binare a lungimii numelui fiecarui tenismen\n");
-		printf("5. afisare in ord crescatoare dupa lungimea numelui fiecarui tenismen\n");
-		printf("6. copierea intr un tabel de pointeri a lungimii fiecarui nume de tenismen\n");
-		printf("7. afisarea sub forma de matrice a lungimii numelui cel mai mare in colturile de jos, iar in rest 0\n");
-		printf("0.exit\n");
-
-		scanf("%d", &opt);
-
-		switch (opt) {
-			case 0: exit(1);
-			case 1: afisare_sir(tenis, n); break;
-			case 2: ord_alf(tenis, n);  break;
-			case 3: ord_INValf(tenis, n);  break;
-			case 4: nume_binar(tenis,n); break;
-			case 5: ord_crescLung(tenis, n);  break;
-			case 6: tabelPointeriLungime(tenis, n); break;
-			case 7: matriceMax(tenis, n);  break;
-			default: printf("opt gresita\n");
-		}
-	}
-	return 0;
-}
+﻿////a) Şterge dintr - un şir de caractere un subşir specificat prin poziţie şi lungime.
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h> //pt stringuri
+//#include<ctype.h> //pt functii ex toupper
+//int validare(char nume[21]) {
+//	int ok = 0, i;
+//	if (nume[0] >= 'A' && nume[0] <= 'Z') {
+//		for (i = 1; i < strlen(nume); i++) {
+//			if (!(nume[i] >= 'a' && nume[i] <= 'z')) {
+//				ok++;
+//			}
+//		}
+//		if (ok == 0) {
+//			return 1;
+//		}
+//		else {
+//			return 0;
+//		}
+//	}
+//	else {
+//		return 0;
+//	}
+//
+//}
+//void citire_sir(char** tenis, int n) {
+//	int i;
+//	char nume[21];
+//	for (i = 0; i < n; i++) {
+//		do {
+//			printf("dati nume: ");
+//			scanf("%s", nume);
+//		} while (validare(nume) == 0);
+//		tenis[i] = (char*)malloc(strlen(nume) + 1); //ii aloc memorie fiecarui cuvant din sir
+//
+//		if (tenis[i] == NULL) {
+//			printf("Eroare alocare memorie");
+//			exit(0);
+//		}
+//		else {
+//			strcpy(tenis[i], nume); //bag in sir fiecare nume citit
+//		}
+//	}
+//}
+//
+//void afisare_sir(char** tenis, int n) {
+//	int i;
+//	for (i = 0; i < n; i++) {
+//		printf("%s ", *(tenis + i));
+//	}
+//	printf("\n");
+//}
+//void ord_alf(char** tenis, int n) {
+//	int i,j;
+//	char aux[21];
+//	for(i=0;i<n;i++)
+//		for (j = i + 1; j < n; j++) 
+//			if (strcmp(*(tenis+i), *(tenis + j)) > 0) {
+//				strcpy(aux, *(tenis + i));
+//				strcpy(*(tenis + i), *(tenis + j));
+//				strcpy(*(tenis + j),aux);
+//			}
+//	
+//	for (i = 0; i < n; i++) {
+//		printf("%s ", *(tenis + i));
+//	}
+//	printf("\n");
+//}
+//
+//void ord_INValf(char** tenis, int n) {
+//	int i, j;
+//	char aux[21];
+//	for (i = 0; i < n; i++)
+//		for (j = i + 1; j < n; j++)
+//			if (!(strcmp(*(tenis + i), *(tenis + j)) > 0)) {
+//				strcpy(aux, *(tenis + i));
+//				strcpy(*(tenis + i), *(tenis + j));
+//				strcpy(*(tenis + j), aux);
+//			}
+//
+//	for (i = 0; i < n; i++) {
+//		printf("%s ", *(tenis + i));
+//	}
+//	printf("\n");
+//}
+//
+//void nume_binar(char** tenis, int n) {
+//	int i,aux;
+//	for (i = 0; i < n; i++) {
+//		aux = strlen(*(tenis + i));
+//		while (aux > 0) {
+//			printf("%d", aux % 2); 
+//			aux = aux / 2;
+//		}
+//		printf("\n");
+//	}
+//	printf("\n");
+//}
+//
+//void ord_crescLung(char** tenis, int n) {
+//	int i, j;
+//	char aux[21];
+//	for (i = 0; i < n; i++)
+//		for (j = i + 1; j < n; j++)
+//			if ( strlen(*(tenis + i))  > strlen(*(tenis + j)) ) {
+//				strcpy(aux, *(tenis + i));
+//				strcpy(*(tenis + i), *(tenis + j));
+//				strcpy(*(tenis + j), aux);
+//			}
+//
+//	for (i = 0; i < n; i++) {
+//		printf("%s ", *(tenis + i));
+//	}
+//	printf("\n");
+//}
+//
+//void tabelPointeriLungime(char** tenis, int n) {
+//	int* tabel;
+//	int i;
+//	tabel = (int*)malloc(n * sizeof(int));
+//	
+//	for (i = 0; i < n; i++) {
+//		*(tabel + i) = strlen(*(tenis+i));
+//	}
+//
+//	for (i = 0; i < n; i++) {
+//		printf("%d ", *(tabel + i));
+//	}
+//	printf("\n");
+//}
+//
+//void matriceMax(char** tenis, int n) {
+//	int max = strlen(*(tenis + 0)) , i,j;
+//
+//	for (i = 0; i < n; i++) {
+//		if (max < strlen(*(tenis + i)) ) {
+//			max = strlen(*(tenis + i));
+//		}
+//	}
+//
+//	for (i = 0; i < max; i++) {
+//		for (j = 0; j < max; j++) {
+//			if ((i == max - 1 && j == 0) || (i == max - 1 && j == max - 1)) {
+//				printf("%d ", max);
+//			}
+//			else {
+//				printf("0 ");
+//			}
+//		}
+//		printf("\n");
+//	}
+//	//printf("%d",max);
+//	printf("\n");
+//}
+//
+//
+//int main() {
+//	//fie un sir fomrat din max 100 nume de tenismeni, realizati un program C in care se citesc numele de tenismen de la tastatura apoi realizati un meniu interactiv cu urm optiuni
+//	//0. iesire
+//	//1. afisare tenismeni cititi
+//	//2. afisare tenismeni in ord alfabetica
+//	//3. afisare tenismeni in ord invers alfabetica
+//	//4. afisarea valorii binare a lungimii numelui fiecarui tenismen
+//	//5. afisare in ord crescatoare dupa lungimea numelui fiecarui tenismen
+//	//6. copierea intr un tabel de pointeri a lungimii fiecarui nume de tenismen
+//	//7. afisarea sub forma de matrice a lungimii numelui cel mai mare in colturile de jos, iar in rest 0
+//	//OBS: numele este validat fiind citit corect daca incepe cu litera mare, iar in rest contine doar litere mici
+//	//     se pot citii max 100 nume de tensmen, iar fiecare tenismne are max 20 car.
+//	//     se va folosi alocarea dinamica de memorie
+//	//     pt fiecare  punct se va face o functie
+//	int n,opt;
+//	char** tenis;
+//
+//	do {
+//		scanf("%d", &n);
+//	} while (n < 0 || n > 100);
+//	
+//	tenis = (char**)malloc(n * sizeof(char*)); //aloc memorie
+//
+//	if (tenis == NULL) {
+//		printf("Eroare alocare memorie");
+//		exit(0);
+//	}	
+//
+//	citire_sir(tenis, n);
+//
+//
+//	while (1) {
+//		printf("1. afisare tenismeni cititi \n");
+//		printf("2. afisare tenismeni in ord alfabetica\n");
+//		printf("3. afisare tenismeni in ord invers alfabetica\n");
+//		printf("4. afisarea valorii binare a lungimii numelui fiecarui tenismen\n");
+//		printf("5. afisare in ord crescatoare dupa lungimea numelui fiecarui tenismen\n");
+//		printf("6. copierea intr un tabel de pointeri a lungimii fiecarui nume de tenismen\n");
+//		printf("7. afisarea sub forma de matrice a lungimii numelui cel mai mare in colturile de jos, iar in rest 0\n");
+//		printf("0.exit\n");
+//
+//		scanf("%d", &opt);
+//
+//		switch (opt) {
+//			case 0: exit(1);
+//			case 1: afisare_sir(tenis, n); break;
+//			case 2: ord_alf(tenis, n);  break;
+//			case 3: ord_INValf(tenis, n);  break;
+//			case 4: nume_binar(tenis,n); break;
+//			case 5: ord_crescLung(tenis, n);  break;
+//			case 6: tabelPointeriLungime(tenis, n); break;
+//			case 7: matriceMax(tenis, n);  break;
+//			default: printf("opt gresita\n");
+//		}
+//	}
+//	return 0;
+//}
---
+++ ipc_laborator2/lab9_3.c
@@ -0,0 +1,178 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h> //pt stringuri
+#include<ctype.h> //pt functii ex toupper
+#include <windows.h> //pt timp
+void afisare(char cnp[14]) {
+	printf("%s \n", cnp);
+}
+
+void cnp_gender(char cnp[14]) {
+	int s = cnp[0] - '0'; 
+	if (s % 2 == 0) {
+		printf("Feminin \n");
+	}
+	else {
+		printf("Masculin \n");
+	}
+}
+
+void cnp_dataNastere(char cnp[14]) {
+	int aa1 = cnp[1] - '0', aa2 = cnp[2] - '0', aa, an;
+	char month[13][50] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+	aa = aa1 * 10 + aa2; //an (ultimele 2 caractere)
+
+	int ll1 = cnp[3] - '0', ll2 = cnp[4] - '0', ll;
+	ll = ll1 * 10 + ll2; //luna
+
+	int zz1 = cnp[5] - '0', zz2 = cnp[6] - '0', zz;
+	zz = zz1 * 10 + zz2; //ziua
+
+	if (aa >= 0 && aa <= 22) {
+		if (aa >= 0 && aa <= 9) {
+			an = 200 * 10 + aa;
+		}
+		else {
+			an = 20 * 100 + aa;
+		}
+	}
+	else {
+		an = 19 * 100 + aa;
+	}
+	printf("%d %s %d \n",zz, month[ll-1], an);
+}
+
+void cnp_varsta(char cnp[14]) {
+	int aa1 = cnp[1] - '0', aa2 = cnp[2] - '0', aa, an;
+	SYSTEMTIME t;
+	GetLocalTime(&t);
+
+	int ll1 = cnp[3] - '0', ll2 = cnp[4] - '0', ll;
+	ll = ll1 * 10 + ll2; //luna
+
+	int zz1 = cnp[5] - '0', zz2 = cnp[6] - '0', zz;
+	zz = zz1 * 10 + zz2; //ziua
+
+	aa = aa1 * 10 + aa2; //an
+
+	if (aa >= 0 && aa <= 22) {
+		if (aa >= 0 && aa <= 9) {
+			an = 200 * 10 + aa;
+		}
+		else {
+			an = 20 * 100 + aa;
+		}
+	}
+	else {
+		an = 19 * 100 + aa;
+	}
+
+	if (t.wMonth < ll) {
+		printf("%d ani\n", t.wYear - an - 1);
+	}
+	else if(t.wMonth == ll && zz > t.wDay) {
+		printf("%d ani\n", t.wYear - an - 1);
+	}
+	else {
+		printf("%d ani\n", t.wYear - an);
+	}
+}
+
+void suma_cnp(char cnp[14]) {
+	int i, s=0;
+
+	for (i = 0; i < strlen(cnp); i++) {
+		s += cnp[i] - '0';
+	}
+	printf("Baza 2: ");
+	for (i = 31; i >= 0; i--) {
+		if (((s >> i) & 1) == 0)
+			printf("0");
+		else
+			printf("1");
+	}
+	printf("\n");
+	
+	printf("Baza 8: %#o \n", s);
+	printf("Baza 10: %d \n", s);
+	printf("Baza 16: %#X \n", s);
+}
+
+int validare_cnp(char cnp[14]) {
+	int is_nr = 0, i;
+
+	int s = cnp[0] - '0'; //primul caracter il transform in cifra(int);
+
+	int ll1 = cnp[3] - '0', ll2 = cnp[4] - '0', ll; 
+	ll = ll1 * 10 + ll2; //luna
+
+	int zz1 = cnp[5] - '0', zz2 = cnp[6] - '0', zz;
+	zz = zz1 * 10 + zz2; //ziua
+
+	int jj1 = cnp[7] - '0', jj2 = cnp[8] - '0', jj;
+	jj = jj1 * 10 + jj2; //judet/sector
+
+	int nnn1 = cnp[9] - '0', nnn2 = cnp[10] - '0', nnn3 = cnp[11] - '0',nnn;
+	nnn = (nnn1 * 10 + nnn2)*10 + nnn3;  // numarul secvential
+
+	int c = cnp[12] - '0'; //componenta de control
+	
+
+
+	for (i = 0; i < strlen(cnp); i++) {  //verificare daca e compus numai din cifre;
+		if (!(cnp[i] >= '0' && cnp[i] <= '9')) {
+			is_nr = 1;
+		}
+	}
+	
+	if (strlen(cnp) == 13 && is_nr==0 && (s>=1 && s<=8) && (ll>=1 && ll<=12) && (zz >= 1 && zz <= 30) && (jj >= 1 && jj <= 48) && (nnn>=1 && nnn<=999) && (c >=1 && c<=9)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+int main() {
+	//problema CNP
+	int opt;
+	char cnp[14];
+	do {
+		scanf("%s", cnp);
+	} while (validare_cnp(cnp)==0);
+
+	while (1) {
+		printf("0. Iesire \n");
+		printf("1. Afișarea CNP-ului. \n");
+		printf("2. Să se afișeze dacă CNP-ul citit este a unei persoane de sex masculin sau feminin \n");
+		printf("3. Afișați data de naștere a persoanei. \n");
+		printf("4. Afișați vârsta persoanei, calculată la data curentă. \n");
+		printf("5. Să se calculeze suma cifrelor din CNP, apoi să se afișeze suma în baza 2, 8, 10 și 16. \n");
+
+		printf("Dati optiune:");
+		scanf("%d", &opt);
+
+		if (opt == 0) {
+			exit(1);
+		}
+		else if (opt == 1) {
+			afisare(cnp);
+		}
+		else if (opt == 2) {
+			cnp_gender(cnp);
+		}
+		else if (opt == 3) {
+			cnp_dataNastere(cnp);
+		}
+		else if (opt == 4) {
+			cnp_varsta(cnp);
+		}
+		else if (opt == 5) {
+			suma_cnp(cnp);
+		}
+		else {
+			printf("Optiune gresita! \n");
+		}
+	}
+	return 0;
+}
